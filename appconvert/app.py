f738cd42c6ec84884583507789721210b864e58a
Alexander Marques <alexandermarquesoficial@gmail.com>
2022-01-30 14:01:05 -0300

    Adicionado gerenciamento de configurações (Dynaconf)

--- appconvert/app.py
+++ appconvert/app.py
@@ -1,6 +1,16 @@
 from flask import Flask
+from appconvert.ext import configuration
 
 
-def create_app():
+def minimal_app(**config):
     app = Flask(__name__, static_folder="blueprints/webui/static")
+    configuration.init_app(app, **config)
+
+    return app
+
+
+def create_app(**config):
+    app = minimal_app(**config)
+    app.config.load_extensions()
+
     return app
