202a69a30b9624bb6bc90a0f9682d7903ef73b8d
Alexander Marques <alexandermarquesoficial@gmail.com>
2022-03-10 21:14:32 -0300

    refactor(pyproject.toml): extensão marshmallow-sqlalchemy para integrar com flask-marshmallow e Flask-SQLAlchemy

--- appconvert/ext/serialyzer.py
+++ appconvert/ext/serialyzer.py
@@ -1,14 +1,21 @@
 from flask_marshmallow import Marshmallow
-from marshmallow import Schema, fields
-from appconvert.models import Category
+from marshmallow import Schema
+from numerize import numerize
 
 ma = Marshmallow()
 
 
 class TokenSchema(ma.Schema):
     class Meta:
-        # Fields to expose
-        fields = ("currency", "symbol", "name", "logo_url","marketcap", "price", "blockchain")
+        fields = (
+            "currency",
+            "symbol",
+            "name",
+            "logo_url",
+            "marketcap",
+            "price",
+            "blockchain",
+        )
 
 
 token_schema = TokenSchema()
